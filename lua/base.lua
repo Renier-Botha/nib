c9d8c02fbedb89041a607c892e28cbba548e242b
Renier Botha <renierb00@gmail.com>
2026-07-04 07:11:52 +0200

    feat: Add file path copy keybindings

--- lua/base.lua
+++ lua/base.lua
@@ -1,7 +1,6 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
 
-
 vim.opt.undofile = true
 
 vim.opt.splitbelow = true
@@ -17,4 +16,15 @@ vim.opt.shiftwidth = 0 -- set to 0 to default to tabstop value
 
 vim.opt.clipboard = "unnamedplus"
 
-vim.keymap.set('n', '<leader>rn', ':set relativenumber!<CR>', { desc = 'Toggle relative line numbers', silent = true })
+vim.keymap.set("n", "<leader>rn", ":set relativenumber!<CR>", { desc = "Toggle relative line numbers", silent = true })
+
+-- Copy file path
+vim.keymap.set("n", "<leader>fp", function()
+	vim.fn.setreg("+", vim.fn.expand("%"))
+end, { desc = "Copy relative file path" })
+vim.keymap.set("n", "<leader>fa", function()
+	vim.fn.setreg("+", vim.fn.expand("%:p"))
+end, { desc = "Copy absolute file path" })
+vim.keymap.set("n", "<leader>ft", function()
+	vim.fn.setreg("+", vim.fn.expand("%:t"))
+end, { desc = "Copy file name" })
