692c8c26998662f123734ea84887d7e465a047fc
Renier Botha <renierb00@gmail.com>
2026-07-15 15:06:52 +0200

    feat: auto complete bullets plugin

---
+++ lua/plugins/bullets.lua
@@ -0,0 +1,6 @@
+-- https://github.com/bullets-vim/bullets.vim
+-- Auto-continues bullet lists (-, *, 1., etc.) on Enter in markdown files.
+return {
+	"bullets-vim/bullets.vim",
+	ft = { "markdown", "text", "gitcommit" },
+}
